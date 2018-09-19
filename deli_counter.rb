3a8310fbeebb8c48a158f27c60d4accb67f8012a
Dimanche Jhon Steeve <jsteeve@noukod.com>
2018-09-19 18:00:59 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,11 @@
 # Write your code here.
     katz_deli = []
-    
+
 def line(line_array)
   if names_array.empty?
     puts "The line is currently empty."
   else
-    names_array.each_with_index do |e, i|  end
+    names_array.each_with_index do |e, i| puts "Hello,  #{e}. You are number #{i+1} in line." end
   end
 end
 
@@ -13,3 +13,5 @@ def take_a_number(line_array, name)
   puts "Welcome,  #{name}. You are number #{line_array.length} in line."
   line_array << name
 end
+
+def now_serving
