15e158dbad62f7de48b874bcf0ed207436e7712b
Dimanche Jhon Steeve <jsteeve@noukod.com>
2018-09-19 17:45:10 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,9 @@
 # Write your code here.
+
+def line(names_array)
+  if names_array.empty?
+    puts "The line is currently empty."
+  else
+    names_array.each_with_index do |e, i| puts "Hello #{e}. #{i} is your place in line" end
+  end
+end
